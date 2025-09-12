df0cab4e10d009cd40206cd1c889d32d8252e21b
Elmosius Suli <elmosius18@gmail.com>
2025-09-12 21:42:12 +0700

    feat: add lenis smooth scroll

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import gsap from 'gsap';
-import Navbar from './components/Navbar';
-import {useScrollLock} from "./utils/scrollLock.ts";
+import { onMounted, ref, watchEffect } from "vue";
+import gsap from "gsap";
+import Navbar from "./components/Navbar";
+import { useScrollLock } from "./utils/scrollLock.ts";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { VueLenis } from "lenis/vue";
 
+const lenisRef = ref();
 const { lock, unlock } = useScrollLock();
 const main = ref<HTMLElement | null>(null);
 const openNav = ref(false);
@@ -17,24 +20,66 @@ const toggleNav = () => {
 
   if (openNav.value) {
     lock();
-    tl.to(main.value, { rotation: 10, x: 300, y: 450, scale: 1.5, duration: 1.5, ease: 'power4.inOut', delay: 0.1 });
+    tl.to(main.value, {
+      rotation: 10,
+      x: 300,
+      y: 450,
+      scale: 1.5,
+      duration: 1.5,
+      ease: "power4.inOut",
+      delay: 0.1,
+    });
   } else {
     unlock();
-    tl.to(main.value, { rotation: 0, x: 0, y: 0, scale: 1, duration: 1.5, ease: 'power4.inOut' });
+    tl.to(main.value, {
+      rotation: 0,
+      x: 0,
+      y: 0,
+      scale: 1,
+      duration: 1.5,
+      ease: "power4.inOut",
+    });
   }
 };
+
+watchEffect((onInvalidate) => {
+  if (!lenisRef.value?.lenis) return;
+  lenisRef.value.lenis.on("scroll", ScrollTrigger.update);
+
+  function update(time: number) {
+    lenisRef.value.lenis.raf(time * 1000);
+  }
+
+  gsap.ticker.add(update);
+  gsap.ticker.lagSmoothing(0);
+  onInvalidate(() => {
+    gsap.ticker.remove(update);
+  });
+});
+onMounted(() => {
+  gsap.registerPlugin(ScrollTrigger);
+});
 </script>
 
 <template>
+  <VueLenis root ref="lenisRef" :options="{ autoRaf: false }" />
+
   <Navbar @toggle-nav="toggleNav" :open-nav="openNav" />
 
   <main class="relative overflow-hidden origin-top-left" ref="main">
     <section class="relative h-screen overflow-hidden bg-amber-100" id="home">
-      <h1 class="text-8xl h-screen font-bold flex justify-center items-center">hello world</h1>
+      <h1 class="text-8xl h-screen font-bold flex justify-center items-center">
+        hello world
+      </h1>
     </section>
 
-    <section class="relative h-screen overflow-hidden bg-amber-400" id="architecture">
-      <h1 class="text-8xl h-screen font-bold flex justify-center items-center">hello world</h1>
+    <section
+      class="relative h-screen overflow-hidden bg-amber-400"
+      id="architecture"
+    >
+      <h1 class="text-8xl h-screen font-bold flex justify-center items-center">
+        hello world
+      </h1>
     </section>
   </main>
 </template>
