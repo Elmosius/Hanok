74992cb1af3b7499d30d05a0ae91bdd16431cb44
Elmosius <2272008@maranatha.ac.id>
2025-09-17 12:10:34 +0700

    feat: make animation title Hero

--- src/components/Hero/Hero.vue
+++ src/components/Hero/Hero.vue
@@ -3,16 +3,28 @@ import { IMAGE_HERO } from '../../constans';
 import gsap from 'gsap';
 import { onBeforeUnmount, onMounted, ref } from 'vue';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-const imgHeroRef = ref<HTMLElement | null>(null);
 
-let tween: any = null;
+const imgHeroRef = ref<HTMLElement | null>(null);
+const titleRef = ref<HTMLElement | null>(null);
+
+let tl: GSAPTimeline | null = null;
+let tween: GSAPTween | null = null;
 
 onMounted(() => {
   gsap.registerPlugin(ScrollTrigger);
 
-  if (!imgHeroRef.value) return;
+  if (!imgHeroRef.value || !titleRef.value) return;
 
-  tween = gsap.timeline().fromTo(
+  tween = gsap.from(titleRef.value.children, {
+    y: 100,
+    rotate: 5,
+    clipPath: 'inset(0% 0% 100% 0%)',
+    duration: 1.25,
+    stagger: 0.5,
+    ease: 'power4.out',
+  });
+
+  tl = gsap.timeline().fromTo(
     imgHeroRef.value,
     {
       yPercent: -15,
@@ -32,17 +44,18 @@ onMounted(() => {
 });
 
 onBeforeUnmount(() => {
-  tween?.scrollTrigger?.kill();
   tween?.kill();
+  tl?.scrollTrigger?.kill();
+  tl?.kill();
 });
 </script>
 
 <template>
   <section class="relative h-screen lg:h-[150vh] overflow-hidden" id="home">
     <div class="w-full h-full relative">
-      <div class="h-full px-15 md:px-30 container mx-auto absolute inset-0 z-10 flex flex-col gap-y-2 justify-center">
-        <h1 class="text-5xl md:text-7xl lg:text-9xl text-primary font-medium">The Beauty of Hanok</h1>
-        <p class="md:text-xl lg:text-3xl text-primary/95 font-light">
+      <div class="h-full px-15 md:px-30 container mx-auto absolute inset-0 z-10 flex flex-col gap-y-2 justify-center" ref="titleRef">
+        <h1 class="text-5xl md:text-7xl lg:text-9xl text-primary font-medium will-change-transform">The Beauty of Hanok</h1>
+        <p class="md:text-xl lg:text-3xl text-primary/95 font-light will-change-transform">
           Timeless Korean traditional houses that embrace <br />
           warmth and harmony
         </p>
